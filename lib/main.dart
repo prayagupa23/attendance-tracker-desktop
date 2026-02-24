8723d13403ec153b5dbd2ec095982d095ed45983
Prayag <prayagupadhyaya23@gmail.com>
2026-02-25 00:26:37 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Final Year Project Desktop',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Color(0xFFA50C22)),
+      ),
+      home: const MyHomePage(title: 'Final Year Project Desktop'),
+      debugShowCheckedModeBanner: false
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('App under development.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
